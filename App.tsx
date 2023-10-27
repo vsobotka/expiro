efe0cb0dfcb24594ba6cad82c07b8cd740fb3d7d
Václav Sobotka <sobotka.vaclav@gmail.com>
2023-10-27 23:03:15 +0200

    Add navigation and prettier to project

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
-import {
-  SafeAreaView,
-  FlatList,
-} from 'react-native';
-import {globalStyle} from "./src/Styles/Global";
+import { SafeAreaView, FlatList } from "react-native";
+import { globalStyle } from "./src/Styles/Global";
 import React from "react";
-import {Item} from "./src/Components/Item";
-import {DATA} from "./src/Storage/Data";
+import { Item } from "./src/Components/Item";
+import { DATA } from "./src/Storage/Data";
+import { NavigationContainer } from "@react-navigation/native";
 
 const App = () => {
   return (
-    <SafeAreaView style={globalStyle.container}>
-      <FlatList
-        data={DATA}
-        renderItem={({item}) => <Item {...item}/>}
-        keyExtractor={item => item.id}
-      />
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView style={globalStyle.container}>
+        <FlatList
+          data={DATA}
+          renderItem={({ item }) => <Item {...item} />}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
+    </NavigationContainer>
   );
 };
 
