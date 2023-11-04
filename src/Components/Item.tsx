e1d004d2ecf2c06e58897cf26e1aa949f7a3230c
Václav Sobotka <sobotka.vaclav@gmail.com>
2023-11-04 15:43:50 +0100

    Show simple progress bar when below 20 days

--- src/Components/Item.tsx
+++ src/Components/Item.tsx
@@ -2,8 +2,9 @@ import { Text, TouchableOpacity, View } from "react-native";
 import { globalStyle } from "../Styles/Global";
 import { Badge } from "./Badge";
 import { ItemType } from "../Storage/Data";
+import * as Progress from "react-native-progress";
 
-type ItemProps = { data: ItemType, onClick: () => void };
+type ItemProps = { data: ItemType; onClick: () => void };
 
 export function Item({ data, onClick }: ItemProps) {
   const today = new Date();
@@ -16,6 +17,9 @@ export function Item({ data, onClick }: ItemProps) {
       <View style={globalStyle.item}>
         <Badge daysLeft={daysLeft} />
         <Text style={globalStyle.title}>{data.title}</Text>
+        {daysLeft >= 0 && daysLeft <= 20 && (
+          <Progress.Bar progress={daysLeft / 20} width={200} />
+        )}
       </View>
     </TouchableOpacity>
   );
